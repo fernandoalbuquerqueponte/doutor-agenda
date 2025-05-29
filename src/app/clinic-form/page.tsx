954a26839138923002e886a2ea9c1ebe78dcd079
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-05-29 01:09:04 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,29 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = async () => {
+  return (
+    <div>
+      <Dialog open>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicionar clínica</DialogTitle>
+            <DialogDescription>
+              Adicionar clínica para continuar
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
