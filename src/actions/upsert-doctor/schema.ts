486f796684e84e6bdf615833eae1fe9a8fe7ae1f
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-06-02 10:42:06 -0300

    feat: ensure doctor is created

---
+++ src/actions/upsert-doctor/schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const upsertDoctorSchema = z
+  .object({
+    id: z.string().uuid().optional(),
+    //eu tirei o clinicId  daqui se der erro já sabe clinicId: z.string().uuid(),
+    name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+    specialty: z
+      .string()
+      .trim()
+      .min(1, { message: "Especialidade é obrigatória" }),
+    appointmentPriceInCents: z
+      .number()
+      .min(1, { message: "Preço da consulta é obrigatório" }),
+    availableFromWeekDay: z.number().min(0).max(6),
+    availableToWeekDay: z.number().min(0).max(6),
+    availableFromTime: z
+      .string()
+      .trim()
+      .min(1, { message: "Hora de início é obrigatória" }),
+    availableToTime: z
+      .string()
+      .trim()
+      .min(1, { message: "Hora de término é obrigatória" }),
+  })
+  .refine(
+    (data) => {
+      return data.availableFromTime < data.availableToTime;
+    },
+    {
+      message:
+        "O horário de início não pode ser anterior ao horário de término",
+      path: ["availableToTime"],
+    },
+  );
+
+export type UpsertDoctorSchema = z.infer<typeof upsertDoctorSchema>;
